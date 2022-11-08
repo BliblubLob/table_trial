7a315d249753b7ff62cae7f0b9b7f2533a9cacc5
HUANG YUXUAN <huangyuxuan121122@gmail.com>
2022-11-09 01:40:47 +0800

    tested with mock api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import MyTable from "./table";
 import TableForm from "./table_form";
+import axios from 'axios'
 
 
-const data =[
+//used for testing without mock api
+var data = [
   {
     "_id": "628dc621adff4647dfdc397b",
     "name": "Test-Dasboard",
@@ -17,30 +19,46 @@ const data =[
 ]
 
 
-export const TableInfo = React.createContext()
 
+//creating context
+export const TableInfo = React.createContext()
 
 function App() {
 
-  const [table_data, set_table_data] = useState(data)
-  const [form_state, set_form_state] = useState({show_form:false,index:0})
+  //for preventing rendering before api replies
+  const [loading, set_loading] = useState(true)
+  const [table_data, set_table_data] = useState(data) // or use usestate(data_list[0] for mock api)
+  const [form_state, set_form_state] = useState({ show_form: false, index: 0 })
+
+
+  //mock api using postman, pls change the url if using other mock api
+  axios.get('https://50ddaebf-7649-4dab-9077-c95f5672cc4b.mock.pstmn.io').then(
+    function (res) {
+      console.log(res.data.data)
+      set_loading(false)
+    }
+  )
 
   //simulate api call and pass data when api returns data
-  const value = {table_data, set_table_data}
-  const value2 = {form_state, set_form_state}
+  const value = { table_data, set_table_data }
+  const value2 = { form_state, set_form_state }
 
+  if (loading){
+    return null
+  }
+  else{
+    return (
+      <div className="App">
   
+        <TableInfo.Provider value={{ value, value2 }}>
+          <MyTable />
+          {form_state.show_form ? <TableForm /> : null}
+        </TableInfo.Provider>
+  
+      </div>
+    );
+  }
 
-  return (
-    <div className="App">
-
-      <TableInfo.Provider value={{value, value2}}>
-        <MyTable />
-        { form_state.show_form ? <TableForm /> : null }
-      </TableInfo.Provider>
-
-    </div>
-  );
 }
 
 export default App;
